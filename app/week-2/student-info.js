798f7d0116e744d9642eaf45944765052aed7438
Rgh2k4 <146884367+Rgh2k4@users.noreply.github.com>
2024-09-17 10:20:25 -0600

    Added week 2 page and student info and modified appPage

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+
+import Link from 'next/link'
+
+ function StudentInfo(){
+    return(
+        <div>
+            <h2>Robert Hansen</h2>
+            <Link href="https://github.com/Rgh2k4/cprg306-assaignments">My Github repository</Link>
+        </div>
+    )
+}
+
+export default StudentInfo;
